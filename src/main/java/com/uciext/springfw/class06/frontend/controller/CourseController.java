14171598e64c8c2af34a041fbe1683ac21f31084
clawrence <klamach@gmail.com>
2017-08-21 17:25:32 -0400

    Commit

---
+++ src/main/java/com/uciext/springfw/class06/frontend/controller/CourseController.java
@@ -0,0 +1,53 @@
+package com.uciext.springfw.class06.frontend.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uciext.springfw.class06.courses.service.CourseService;
+import com.uciext.springfw.class06.courses.model.Course;
+
+@Controller
+@RequestMapping("/course")
+public class CourseController {
+		
+	private CourseService courseService;
+	
+	@Inject
+	public void setCourseService(CourseService courseService) {
+		this.courseService = courseService;
+	}
+	
+	@RequestMapping("/list")
+	public ModelAndView courseList(Model model) {
+		System.out.println("======= in courseList");
+		List<Course> courses = courseService.getCourses();
+		model.addAttribute("courseList", courses);
+		return new ModelAndView("course/courseList");
+	}
+
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String addCourse(Model model) {
+		System.out.println("======= in addCourse");
+		model.addAttribute(new Course());
+		return "course/addCourse";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String addCourseSave(@Valid Course course, BindingResult bindingResult) {
+		System.out.println("======= in addCourseSave");
+		if(bindingResult.hasErrors()){
+			return "course/addCourse";
+		}
+		courseService.insertCourse(course);
+		return "redirect:/course/list.html";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/course/addCourse.jsp
@@ -0,0 +1,50 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form" %>
+
+
+
+<html>
+<head>
+    <title>Add Course</title>
+</head>
+<body>
+    Add New Course:
+<br>
+<sf:form method="POST" modelAttribute="course">
+<table border="0">
+	<tr> 
+		<th><label for="course_code">Course Code:</label></th>
+    	<td><sf:input path="courseCode" size="15" id="course_code" /><br/>
+    	        	<sf:errors path="courseCode" cssClass="error" /> 
+
+    	</td> 
+	</tr> 
+	<tr> 
+		<th><label for="course_name">Course Name:</label></th>
+    	<td><sf:input path="courseName" size="20" id="course_name" /><br/>
+    	        	<sf:errors path="courseName" cssClass="error" /> 
+
+    	</td> 
+	</tr> 
+			<tr> 
+		<th><label for="department_id">Department Name:</label></th>
+    	<td><sf:input path="department.departmentId" size="20" id="department_id" /><br/>
+
+    	</td> 
+	</tr> 
+		<tr> 
+		<th><label for="department_name">Department Name:</label></th>
+    	<td><sf:input path="department.departmentName" size="20" id="department_name" /><br/>
+
+    	</td> 
+	</tr> 
+	<tr>
+		<th></th>
+		<td><input type="submit" value="Save" /></td>
+	</tr>
+</table>
+</sf:form>
+    
+</body>
+</html>
+
